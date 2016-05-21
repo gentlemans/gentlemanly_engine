b79d32629f5c972172d92569ec090c7645a02f45
Russell Greene <russellgreene8@gmail.com>
2016-05-21 16:27:34 -0600

    Fix bug where not all the verticies would be loaded

--- src/ge/mesh_asset.cpp
+++ src/ge/mesh_asset.cpp
@@ -33,22 +33,36 @@ mesh_asset::mesh_asset(asset_manager& manager, const std::string& arg_name,
 	if(!scene->HasMeshes()) {
 		throw std::runtime_error("Failed to import file: " + path + " Reason: no meshes");
 	}
-	std::vector<float> locs(scene->mMeshes[0]->mNumVertices * 3);
-	memcpy(locs.data(), scene->mMeshes[0]->mVertices, sizeof(aiVector2D) * scene->mMeshes[0]->mNumVertices);
+	
+	auto& mesh_ref = *scene->mMeshes[0];
+	
+	std::vector<float> locs(mesh_ref.mNumVertices * 3);
+	memcpy(locs.data(), mesh_ref.mVertices, sizeof(aiVector3D) * mesh_ref.mNumVertices);
 	
 	// remove Z's, this is a 2D engine
 	for (size_t index = 2; index < locs.size(); index += 2)
 	{
 		locs.erase(locs.begin() + index);
 	}
+	
+	// build element vector
+	std::vector<glm::uvec3> elements(mesh_ref.mNumFaces * 3);
+	for(auto index_of_face = 0; index_of_face < mesh_ref.mNumFaces; ++index_of_face) {
+		elements[index_of_face] = {
+			mesh_ref.mFaces[index_of_face].mIndices[0],
+			mesh_ref.mFaces[index_of_face].mIndices[1],
+			mesh_ref.mFaces[index_of_face].mIndices[2],
+		};
+	}
+	
 
 	// these reinterpret_casts are safe because glm makes sure to not have any
 	// padding
 	data = std::make_shared<mesh>(reinterpret_cast<glm::vec2*>(locs.data()),
-		reinterpret_cast<glm::vec2*>(locs.data()), 
+		reinterpret_cast<glm::vec2*>(mesh_ref.mTextureCoords), 
 		locs.size(),
-		reinterpret_cast<glm::uvec3*>(scene->mMeshes[0]->mFaces), 
-		scene->mMeshes[0]->mNumFaces);
+		elements.data(), 
+		mesh_ref.mNumFaces);
 
 	// load up material
 	std::string material_asset_path = json_data["material"];
