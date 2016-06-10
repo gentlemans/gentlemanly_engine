d24cfe2cd631fd5f220f904e87715124e42ce2ca
Aun-Ali Zaidi <admin@kodeit.net>
2016-06-10 12:30:21 -0500

    ge/gl.hpp: Fix Linux include order

--- include/ge/gl.hpp
+++ include/ge/gl.hpp
@@ -5,17 +5,16 @@
 	#include <OpenGL/gl3.h>
 	#include <OpenGL/glu.h>
 	#include <OpenGL/gl3ext.h>
-#else
+#elif defined(__linux__)
+        #define GL_GLEXT_PROTOTYPES
 	#include <GL/gl.h>
 	#include <GL/glu.h>
+        #include <GL/glext.h>
+#elif defined(WIN32) | defined(_WIN32)
+        #include <windows.h>
+	#include <gl/gl.h>
+        #include <gl/glew.h>
+        #include "external/glext.h"
+	#include "external/wglext.h"
 #endif
 
-#if defined(__linux__)
-	#define GL_GLEXT_PROTOTYPES
-	#include <GL/glext.h>
-	#include <GL/glew.h>
-#elif defined(WIN32) | defined(_WIN32)
-	#include <windows.h>
-	#include <gl/glew.h>
-	#include "external/glext.h"
-#endif
