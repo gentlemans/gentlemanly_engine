fef69f9a639b5eab37a51ccbfe635bb7142112e2
Aun-Ali Zaidi <admin@kodeit.net>
2016-06-10 11:56:45 -0500

    ge/gl.hpp: Add Platform-specific includes

--- include/ge/gl.hpp
+++ include/ge/gl.hpp
@@ -1,9 +1,21 @@
 #pragma once
 
-#ifdef __linux__
-#define GL_GLEXT_PROTOTYPES
-#include <GL/gl.h>
-#include <GL/glext.h>
-#elif defined WIN32 | _WIN32
-#include <gl/glew.h>
+#if defined(__APPLE__)
+	#include <OpenGL/OpenGL.h>
+	#include <OpenGL/gl3.h>
+	#include <OpenGL/glu.h>
+	#include <OpenGL/gl3ext.h>
+#else
+	#include <GL/gl.h>
+	#include <GL/glu.h>
+#endif
+
+#if defined(__linux__)
+	#define GL_GLEXT_PROTOTYPES
+	#include <GL/glext.h>
+	#include <GL/glew.h>
+#elif defined(WIN32) | defined(_WIN32)
+	#include <windows.h>
+	#include <gl/glew.h>
+	#include "external/glext.h"
 #endif
