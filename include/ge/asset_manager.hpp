9cb4193444c1c96851eac74a65a1920c8f89257c
Russell Greene <russellgreene8@gmail.com>
2016-09-10 20:14:57 -0600

    Move scripts to their own directory, add the start to a asset tutorial

--- include/ge/asset_manager.hpp
+++ include/ge/asset_manager.hpp
@@ -96,7 +96,7 @@ public:
 
 	/// Adds a search path to find assets
 	/// \param path The path to add
-	/// \param priority The priority of the path, the lower the priority the earlier it gets checked
+	/// \param priority The priority of the path, 255 being the highest and 0 being the lowest priority
 	void add_asset_path(std::string path, uint8_t priority = 0)
 	{
 		if (!boost::filesystem::is_directory(path)) {
@@ -202,8 +202,8 @@ private:
 		using namespace std::string_literals;
 
 		std::string abs_path;
-		// acquire absolute path
-		for (auto& priority_and_paths : m_search_paths) {
+		// acquire absolute path; go backwards to make 255 the highest priority
+		for (auto iter  = m_search_paths.rbegin(); iter = m_search_paths.rend(); ++iter) {
 			for (auto& path : priority_and_paths.second) {
 				if (boost::filesystem::is_regular_file(path + "/" + name + "/asset.json")) {
 					abs_path = path + "/" + name;
