6a87b430efe223879ef56b8f4065f7eb8be59925
Russell Greene <russellgreene8@gmail.com>
2016-10-08 07:51:13 -0600

    Fix bug in grid where things didn't show up

--- samples/grid/grid.cpp
+++ samples/grid/grid.cpp
@@ -4,12 +4,12 @@
 
 void grid::initialize(glm::uvec3 size)
 {
-	set_relative_location({-size.x / 2, -size.y / 2});
+	set_relative_location(glm::vec2(-float(size.x) / 2.f, -float(size.y) / 2.f));
 
 	m_size = size;
 
 	towers.resize(size.x * size.y * size.z);
 
-	getActorFromCoord({size.x / 2 + 1, size.y / 2 + 1, 2}) =
-		ge::actor::factory<base>(this, glm::uvec3{size.x / 2 + 1, size.y / 2 + 1, 2}).get();
+	getActorFromCoord({size.x / 2, size.y / 2, 2}) =
+		ge::actor::factory<base>(this, glm::uvec3{size.x / 2, size.y / 2, 2}).get();
 }
