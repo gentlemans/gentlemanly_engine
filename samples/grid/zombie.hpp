79cb9eb8c6aba89623caec48e4c509411bd0561f
Russell Greene <russellgreene8@gmail.com>
2016-10-13 19:49:37 -0600

    Try to fix ci, add zombie

--- samples/grid/zombie.hpp
+++ samples/grid/zombie.hpp
@@ -4,17 +4,26 @@
 #include "grid.hpp"
 #include "toweractor.hpp"
 #include "gridtick_interface.hpp"
+#include <ge/mesh_actor.hpp>
+#include <ge/texture_asset.hpp>
 
 class zombie : public toweractor
 {
+public:
+	
+	ge::mesh_actor* m_mesh;
+	
     void initialize(glm::uvec3 location) {
         toweractor::initialize(location);
 
         add_interface<zombie, gridtick_interface>();
+		
+		m_mesh = factory<ge::mesh_actor>(this, "texturedmodel/textured.meshsettings").get();
+		m_mesh->m_mesh_settings.m_material.m_property_values["Texture"] = m_runtime->m_asset_manager.get_asset<ge::texture_asset>("zombie.texture");
 
     }
 
-    void gridtick() {
-
+    void tick_grid() {
+		std::cout << "Ticked!\n";
     }
 };
