f341a11c4cdadbff9b46fb2f74608879994bdba5
Russell Greene <russellgreene8@gmail.com>
2016-10-15 16:00:39 -0600

    Move dependencies to a build script, broken on windows for now will fix

--- scripts/compile_dependencies.sh
+++ scripts/compile_dependencies.sh
@@ -9,8 +9,8 @@ DEPS_DIR=$GE_DIR/deps
 BUILD_DIR=$DEPS_DIR/build
 INSTALL_DIR=$DEPS_DIR/install
 
-rm -rf $BUILD_DIR
-rm -rf $INSTALL_DIR
+#rm -rf $BUILD_DIR
+#rm -rf $INSTALL_DIR
 
 mkdir -p $BUILD_DIR
 mkdir -p $INSTALL_DIR
@@ -27,4 +27,56 @@ compileft2() {
 	
 }
 
+compileLibRocket() {
+	
+	cd $BUILD_DIR
+	mkdir -p libRocket && cd libRocket
+	
+	cmake ../../libRocket/Build -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+	make
+	make install
+	
+}
+
+compileOGG() {
+	cd $BUILD_DIR
+	mkdir -p ogg && cd ogg
+	
+	cmake ../../ogg -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+	make 
+	make install
+}
+
+compileVorbis() {
+	cd $BUILD_DIR
+	mkdir -p vorbis && cd vorbis
+	
+	cmake ../../vorbis -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DCMAKE_PREFIX_PATH=$INSTALL_DIR
+	make
+	make install
+}
+
+compileBoost() {
+	cd $BUILD_DIR
+	mkdir -p boost && cd boost
+	
+	cmake ../../boost -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR 
+	make 
+	make install
+}
+
+compileOpenAL() {
+	cd $BUILD_DIR
+	mkdir -p openal && cd openal
+	
+	cmake ../../openal-soft -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+	make 
+	make install
+}
+
 compileft2
+compileOGG
+compileVorbis
+compileLibRocket
+compileBoost
+compileOpenAL
