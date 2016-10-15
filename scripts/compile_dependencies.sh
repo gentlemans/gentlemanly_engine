15bf3d5167262c70ad28b778701a888b941b6d7a
Russell Greene <russellgreene8@gmail.com>
2016-10-15 15:20:29 -0600

    Work more on script

--- scripts/compile_dependencies.sh
+++ scripts/compile_dependencies.sh
@@ -2,22 +2,29 @@
 
 set -xe
 
-SCRIPTSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-GEDIR=$SCRIPTSDIR/..
-DEPSDIR=$GEDIR/deps
-BUILDIR=$DEPSDIR/build
+GE_DIR=$SCRIPTS_DIR/..
+DEPS_DIR=$GE_DIR/deps
+BUILD_DIR=$DEPS_DIR/build
+INSTALL_DIR=$DEPS_DIR/install
 
-mkdir -p $BUILDDIR
+rm -rf $BUILD_DIR
+rm -rf $INSTALL_DIR
+
+mkdir -p $BUILD_DIR
+mkdir -p $INSTALL_DIR
 
 compileft2() {
 	
-	cd $BUILDDIR
+	cd $BUILD_DIR
 	
 	mkdir -p ft2 && cd ft2
 	
-	cmake ../../freetype2 -G"Unix Makefiles" -DCMAKE_COMPILE_PREFIX=$
+	cmake ../../freetype2 -G"Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
 	make
 	make install
 	
 }
+
+compileft2
