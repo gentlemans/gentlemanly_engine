4fe6d8f49633d79e011c60e1a32c55f2180d6f1e
Russell Greene <russellgreene8@gmail.com>
2016-10-16 09:08:54 -0600

    Add windows compile scritps, move most of it into cmake to make it better

---
+++ scripts/compile_dependencies_vs.bat
@@ -0,0 +1,14 @@
+TITLE Compiling gentlemanly_engine dependencies for visual studio...
+
+:: Get the location of the script
+PUSHD "%~dp0" >NUL && SET scripts_dir=%CD% && POPD >NUL
+
+:: Set some variables
+SET ge_dir=%scripts_dir%/..
+SET deps_dir=%ge_dir%/deps
+SET build_dir=%deps_dir%/build
+SET install_dir=%deps_dir%/install
+
+cd %build_dir%
+cmake .. -G"Visual Studio 14 Win64"
+cmake --build . --target install
