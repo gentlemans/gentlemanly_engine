7e776591a48c5846b300b15f5c3e876583410fcf
Mattmanskis <msissom8246@bvsd.org>
2016-10-27 22:23:17 -0600

    worked on rand movement brokeAF.jpg though

--- samples/grid/zombie.cpp
+++ samples/grid/zombie.cpp
@@ -50,33 +50,41 @@ void zombie::move_closer_to_center()
 	if (thingsAtPlace.size() == 0)
 		set_grid_location(glm::ivec3{ myLocation.x, myLocation.y, m_level });
 }
-void randomXY(glm::ivec2 &myLocation, int rand1, int rand2)
+std::vector <glm::ivec2> checkNearbyEmpty(glm::ivec2 myLocation)
 {
-	if (rand1 == 0)
+	std::vector <glm::ivec2> emptySquares;
+	for (int x = 0; x < 4; x++)
 	{
-		if (rand2 == 0)
-			myLocation.x++;
-		else
-			myLocation.y++;
-	}
-	else
-	{
-		if (rand2 == 0)
+		if (x == 0)
+		{
 			myLocation.x--;
-		else
+		}
+		if (x == 1)
+		{
+			myLocation.x = myLocation.x + 2;
+		}
+		if (x == 2)
+		{
 			myLocation.y--;
+		}
+		if (x == 3)
+		{
+			myLocation.y = myLocation.y + 2;
+		}
+		auto thingsAtPlace = m_grid->get_actors_from_coord({ myLocation.x, myLocation.y, 2 });
+		if (thingsAtPlace.size() == 0)
+			emptySquares.push_back = thingsAtPlace;
 	}
+	return emptySquares;
 }
 void zombie::move_random()
 {
 	glm::ivec2 myLocation = get_grid_location();
-	int rand1 = m_grid->get_random(0, 1);
-	int rand2 = m_grid->get_random(0, 1);
-	randomXY(myLocation, rand1, rand2);
-	auto thingsAtPlace = m_grid->get_actors_from_coord({ myLocation.x, myLocation.y, 2 });
-	if (thingsAtPlace.size() == 0)
-	{
-		set_grid_location(glm::ivec3{ myLocation.x, myLocation.y, m_level });
-		return
-	}
+	std::vector <glm::ivec2> emptySquares=checkNearbyEmpty(myLocation);
+	if (emptySquares.size == 0)
+		return;
+	int rand = m_grid->get_random(0, emptySquares.size);
+	set_grid_location(glm::ivec3{ emptySquares[rand].x, emptySquares[rand].y, m_level });
+	return;
+
 }
