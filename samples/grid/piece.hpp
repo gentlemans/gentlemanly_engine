f3d32fbdcc2558998cdb90791a645e9c2be5cf6c
Russell Greene <russellgreene8@gmail.com>
2016-11-10 14:24:07 -0700

    Add a first signal!

--- samples/grid/piece.hpp
+++ samples/grid/piece.hpp
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include <boost/signals2.hpp>
+
 #include <ge/actor.hpp>
 #include "grid.hpp"
 
@@ -17,6 +19,11 @@ public:
 	int m_level;
 	std::array<std::vector<piece*>, 4> checkNearbySquares(glm::ivec2 myLocation);
     std::vector<std::vector<piece*>> squares_in_direction(glm::ivec2 myLocation, Directions direction, int range);
+
+    boost::signals2::signal<
+        void(piece* p, glm::ivec3 new_loc, glm::ivec3 old_loc)
+    > sig_move;
+
 	void initialize(glm::ivec3 loc)
 	{
 		m_grid = static_cast<grid*>(get_parent());
@@ -26,8 +33,12 @@ public:
 	}
 	void set_grid_location(glm::ivec3 loc)
 	{
+        glm::ivec3 old = get_grid_location();
+
 		set_relative_location({loc.x, loc.y});
 		m_level = loc.z;
+
+        sig_move(this, get_grid_location(), old);
 	}
 	glm::ivec3 get_grid_location() const
 	{
