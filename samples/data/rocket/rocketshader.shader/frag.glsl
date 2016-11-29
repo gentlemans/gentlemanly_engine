c1e17758d716867975d97a857f8a9e8fa77a8f45
Russell Greene <russellgreene8@gmail.com>
2016-11-29 15:30:43 -0700

    Work on rocket rendering

--- samples/data/rocket/rocketshader.shader/frag.glsl
+++ samples/data/rocket/rocketshader.shader/frag.glsl
@@ -10,9 +10,13 @@ varying vec4 vert_colors;
 
 void main()
 {
-	gl_FragColor = float(use_texture) * texture2D(tex, tex_coords)
-		+ float(1 - use_texture) * vert_colors;
-
+	if(use_texture) {
+		gl_FragColor = texture2D(tex, tex_coords);
+		gl_FragColor += vert_colors * (1 - gl_FragColor.a);
+	} else {
+		gl_FragData = vert_colors;
+	}
+	
 	if(gl_FragColor.a < 0.1) discard;
 
 }
