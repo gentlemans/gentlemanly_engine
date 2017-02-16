ad563e3468be1f811dd5905127f0a4b341829dfb
Russell Greene <russellgreene8@gmail.com>
2017-02-16 13:46:43 -0700

    Stat on stats

---
+++ samples/grid/statsmath.c
@@ -0,0 +1,13 @@
+#include "statsmath.h"
+
+#include <math.h>
+
+float turret_damage_up(float initial, int upgradeval) {
+	
+}
+
+float turret_attack_spped_up(float initial, int upgradeval) {
+	
+}
+
+
---
+++ samples/grid/statsmath.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef GRID_STATS_MATH_HPP
+#define GRID_STATS_MATH_HPP
+
+float turret_damage_up(float initial, int upgradeval);
+
+float turret_attack_spped_up(float initial, int upgradeval);
+
+
+
+#endif // GRID_STATS_MATH_HPP
